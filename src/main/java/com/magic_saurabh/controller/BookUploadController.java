7351f4f4ca4d1cff993d053ab81b21ba6b47e3c4
Saurabh Kumar <kumar.saurabh@magicsw.com>
2025-02-25 11:12:31 +0530

    magic Software

--- src/main/java/com/magic_saurabh/controller/BookUploadController.java
+++ src/main/java/com/magic_saurabh/controller/BookUploadController.java
@@ -1,13 +1,14 @@
 package com.magic_saurabh.controller;
 
 import com.magic_saurabh.service.BookUploadService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.MediaType;
 import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
-
-
 
 
 
@@ -16,18 +17,18 @@ import reactor.core.publisher.Mono;
 public class BookUploadController {
 
     private final BookUploadService bookUploadService;
-
     public BookUploadController(BookUploadService bookUploadService) {
         this.bookUploadService = bookUploadService;
     }
 
-    @PostMapping
+    @PostMapping(value = "/uploadBooks", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary = "Upload books from an Excel file", description = "Uploads books from an Excel file and saves them to the database")
+
     public Mono<Void> uploadFile(@Parameter(
                                         description = "The file to upload",
                                         required = true,
                                         schema = @Schema(type = "string", format = "binary"))
                                      @RequestPart("file") FilePart filePart) {
         return bookUploadService.uploadBooks(filePart);
-
     }
 }
